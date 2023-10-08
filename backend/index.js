409b071a3ff29fa498f705863ddfb8befe758835
meeta haldar <meetahaldar1001@gmail.com>
2023-10-08 18:21:01 +0530

    changed package.json

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,15 @@ const path = require("path");
 const File = require("./model/fileSchema");
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin:
+      "https://file-upload-and-short-link-generation-platform-aoqo.vercel.app/",
+    credentials: true,
+  })
+);
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_STRING, {
   useNewUrlParser: true,
